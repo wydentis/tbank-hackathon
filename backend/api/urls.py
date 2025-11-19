beaecb591fadd0394848c733c6fb96de41ca0c4f
dominator <iamunknownsubject@gmail.com>
2025-11-19 14:44:54 +0300

    basic backend initialized

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from . import views
+urlpatterns = [
+    path('token/',
+         jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
+]
